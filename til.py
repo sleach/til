4472c50c70cdf83df81a7ce983b4a4c51258ce8d
Sean Leach <sleach@wiggum.com>
2020-04-21 06:23:15 -0600

    moved to a single python file

--- til.py
+++ til.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python3
-"""This will build the sqllite db"""
+"""Manage TIL files"""
+import argparse
+import re
+import sys
 from datetime import timezone
 import pathlib
 import git
 import sqlite_utils
+
+
+def update_readme(repo_path, rewrite):
+    """Iterate the MD files and update the main README.md file"""
+    index_re = re.compile(
+        r"<!\-\- index starts \-\->.*<!\-\- index ends \-\->", re.DOTALL
+    )
+    til_db = sqlite_utils.Database(repo_path / "til.db")
+    by_topic = {}
+    for row in til_db["til"].rows_where(order_by="created_utc"):
+        by_topic.setdefault(row["topic"], []).append(row)
+    index = ["<!-- index starts -->"]
+    for topic, rows in by_topic.items():
+        index.append("## {}\n".format(topic))
+        for row in rows:
+            index.append(
+                "* [{title}]({url}) - {date}".format(
+                    date=row["created"].split("T")[0], **row
+                )
+            )
+        index.append("")
+    if index[-1] == "":
+        index.pop()
+    index.append("<!-- index ends -->")
+    if rewrite:
+        readme = repo_path / "README.md"
+        index_txt = "\n".join(index).strip()
+        readme_contents = readme.open().read()
+        readme.open("w").write(index_re.sub(index_txt, readme_contents))
+    else:
+        print("\n".join(index))
 
 
 def created_changed_times(repo_path, ref="master"):
@@ -53,5 +87,25 @@ def build_database(repo_path):
         table.enable_fts(["title", "body"])
 
 
+def main():
+    """Main program for managing args etc."""
+    repo_path = pathlib.Path(__file__).parent.resolve()
+    parser = argparse.ArgumentParser(description="Manage TIL files.")
+    parser.add_argument("--build", action="store_true", help="Build database file")
+    parser.add_argument("--update", action="store_true", help="Update README file")
+    parser.add_argument(
+        "--rewrite", action="store_true", help="Rewrite the README in place"
+    )
+    parser.add_argument("--repo-path", default=repo_path, help="Path to repo directory")
+
+    args = parser.parse_args()
+    if args.build:
+        build_database(args.repo_path)
+    elif args.update:
+        update_readme(args.repo_path, args.rewrite)
+    else:
+        parser.print_help(sys.stderr)
+
+
 if __name__ == "__main__":
-    build_database(pathlib.Path(__file__).parent.resolve())
+    main()
